8b74ba3cae72446f7af32ea34bef43fe12f16d55
shubham <shubhamyadaveer@gmail.com>
2024-12-28 01:03:16 +0530

    Function L Completed

---
+++ alphabetsPattern/L.js
@@ -0,0 +1,18 @@
+// Pattern Printing --- (L)
+
+let num=5;
+
+function patternPrinting(n){
+for(let i=0;i<=n;i++){
+    console.log("*");
+}
+
+
+let temp="";
+for(let i=0;i<n;i++){
+    temp+="*";
+}
+console.log(temp);
+
+}
+patternPrinting(num);
