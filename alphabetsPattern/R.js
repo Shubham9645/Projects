9b0ed187519ee3eb04bae7b5ca4d3f6bf9fd744e
shubham <shubhamyadaveer@gmail.com>
2024-12-28 12:48:10 +0530

    Function R Completed

---
+++ alphabetsPattern/R.js
@@ -0,0 +1,50 @@
+// Pattern Printing --- (R)
+
+let num=5;
+
+function patternPrinting(n){
+    let upperHeight=Math.floor(n/2);
+    let lowerHeight=Math.ceil(n/2);
+
+    let temp="";
+    for(let i=0;i<n;i++){
+        if(i==0 || i==n-1){
+            temp+=" ";
+        }else{
+            temp+="*";
+        }
+    }
+    console.log(temp);
+
+    for(let i=0;i<upperHeight;i++){
+        let temp="";
+        for(let j=0;j<n;j++){
+            if(j==0 || j==n-1){
+                temp+="*";
+            }else{
+                temp+=" ";
+            }
+        }
+        console.log(temp);
+    }
+
+    let temp1="";
+    for(let i=1;i<n;i++){
+        temp1+="*";
+    }
+    console.log(temp1);
+
+    for(let i=0;i<lowerHeight;i++){
+        let  temp="";
+        for(let j=0;j<n;j++){
+            if(j==0 || j==n-1){
+                temp+="*";
+            }else{
+                temp+=" ";
+            }
+        }
+        console.log(temp);
+    }
+
+}
+patternPrinting(num);
